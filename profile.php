d55f139cea8999d1cb8b7e1fedca103ee27fc9c4
miniestaysilent <140136533+miniestaysilent@users.noreply.github.com>
2025-04-08 06:13:49 +0000

    added profile page

---
+++ profile.php
@@ -0,0 +1,24 @@
+<?php
+require_once "vendor/autoload.php";
+
+// Classes used on this page
+use TeamCherry\MusicMuse\App;
+
+// Create app from App class
+$app = new App();
+$site_name = $app -> site_name;
+
+// Create data variables
+$page_title = $site_name . "|". "My Profile";
+
+// Loading the twig template
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+$twig = new \Twig\Environment( $loader );
+$template = $twig -> load( 'profile.twig' );
+
+// Render the ouput
+echo $template -> render( [ 
+    'title' => $page_title,
+    'website_name' => $site_name
+] );
+?>
